bdbb94cdf34aee363272e8cad65dc26e5078e297
LuizHVicari <luizhenrique.vicari@gmail.com>
2023-07-19 15:39:45 -0300

    remove debbug

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,5 @@
 class ArticlesController < ApplicationController
   def show
-    debugger
     @article = Article.find(params[:id])
     @title = @article.title
     @description = @article.description
