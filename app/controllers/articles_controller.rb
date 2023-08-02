6de723ffad02a1fb10ac1753eedfb7d6d1782395
LuizHVicari <luizhenrique.vicari@gmail.com>
2023-08-02 11:26:56 -0300

    drying code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,9 @@
 class ArticlesController < ApplicationController
+
+  before_action :set_article, only: [:show, :edit, :destroy, :update]
+
   def show
-    @article = Article.find(params[:id])
+
     @title = @article.title
     @description = @article.description
   end
@@ -17,7 +20,7 @@ class ArticlesController < ApplicationController
 
 
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
 
     if @article.save
       flash[:notice] = "Article was created successfully"
@@ -31,8 +34,8 @@ class ArticlesController < ApplicationController
 
 
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+
+    if @article.update(article_params)
       puts "aaaaaa"
       flash[:notice] = "Article was updated successfully"
       redirect_to @article
@@ -46,15 +49,24 @@ class ArticlesController < ApplicationController
 
 
   def edit
-    @article = Article.find(params[:id])
+
   end
 
 
   def destroy
-    @article = Article.find(params[:id])
+
     @article.destroy
     redirect_to articles_path
   end
 
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
 
 end
